0e62229425de8c64cddcb0a16126bc64e3089993
Malachi <mmcintosh@planittesting.com>
2021-06-27 12:03:44 +1200

    Slight changes to both function and site To test the Actions.

--- functionApp/src/main/java/com/malachi/Function.java
+++ functionApp/src/main/java/com/malachi/Function.java
@@ -51,5 +51,4 @@ public class Function {
             return resp;
         }
     }
-
 }
--- site/count.js
+++ site/count.js
@@ -7,7 +7,7 @@ window.addEventListener("DOMContentLoaded", (event) => {
 const functionApi = 'https://malachiscvpagecounter.azurewebsites.net/api/GetCVPageCount?id=count';
 
 const getCVPageVisitCount = () => {
-  let count = 2;
+  let count = 2; // default to 2.
   fetch(functionApi)
     .then((response) => {
       let json = response.json();
